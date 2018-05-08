9e71be6eca108795cc0819880d790b65a7c7c090
fukanli <1692885241@qq.com>
2018-05-09 07:02:55 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <myheader></myheader>
+    <div class="tab"> i am tab</div>
+    <div class="content"> i am content</div>
+  </div>
+</template>
+
+<script>
+import Header from './components/header/header.vue';
+export default {
+  components: {
+    Myheader: Header
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>我是header</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {};
+</script>
+
+<style scoped type="stylesheet/stylus">
+
+</style>
